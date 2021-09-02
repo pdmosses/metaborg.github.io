4eb6c514139e53127a000eae84a220923200611d
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2021-09-03 00:47:24 +0200

    Add `gui` inline language

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Spoofax',
+      version='3.0',
+      description='Spoofax: The Language Workbench',
+      author='PL Team',
+      #author_email='gward@python.net',
+      url='https://www.spoofax.dev/',
+      packages=[],
+     )
