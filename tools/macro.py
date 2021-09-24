3cd292b37eb1ffb1083e8c35a7d9c24b495cecad
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2021-09-24 13:58:06 +0200

    Add error, info, trace, debug, yes, no icons

--- tools/macro.py
+++ tools/macro.py
@@ -1,10 +1,17 @@
 from typing import Optional
 
+trace_icon = ":fontawesome-solid-route:"
+debug_icon = ":fontawesome-solid-bug:"
+info_icon = ":fontawesome-solid-info-circle:"
+warning_icon = ":fontawesome-solid-exclamation-triangle:"
+error_icon = ":fontawesome-solid-exclamation-circle:"
+stop_icon = ":fontawesome-solid-times-circle:"
+true_icon = ":fontawesome-solid-check-square:"
+false_icon = ":fontawesome-solid-times-circle:"
+
 windows_icon = ":fontawesome-brands-windows:"
 macos_icon = ":fontawesome-brands-apple:"
 linux_icon = ":fontawesome-brands-linux:"
-warning_icon = ":fontawesome-solid-exclamation-triangle:"
-stop_icon = ":fontawesome-solid-times-circle:"
 
 artifacts_url_base = 'https://artifacts.metaborg.org'
 artifacts_releases_url = f'{artifacts_url_base}/content/repositories/releases/org/metaborg/'
@@ -185,8 +192,16 @@ def define_env(env):
 
 
 def define_macros(vars):
+    vars['trace'] = f"{trace_icon}{{.trace}}"
+    vars['debug'] = f"{debug_icon}{{.debug}}"
+    vars['info'] = f"{info_icon}{{.info}}"
     vars['warning'] = f"{warning_icon}{{.warning}}"
+    vars['error'] = f"{error_icon}{{.error}}"
     vars['stop'] = f"{stop_icon}{{.stop}}"
+
+    # NOTE: `true` and `false` are reserved keywords
+    vars['yes'] = f"{true_icon}{{.true}}"
+    vars['no'] = f"{false_icon}{{.false}}"
 
     vars.os = dict(
         windows=f'{windows_icon} Windows',
